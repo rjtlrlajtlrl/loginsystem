515f7d85b5ec0ee6c65f9baa9171b76bc7bd9b43
itg <itg@DESKTOP-4M2PU23>
2025-01-27 10:08:02 +0900

    1

--- dataset.js
+++ dataset.js
@@ -47,6 +47,7 @@ data_sets.push(d1)
 let d2 = new DataSet("공통모듈구현")//메인타이틀
 
 d2.set_content("공통모듈구현을 작성합니다.")//서브 타이틀
+d2.set_img(0,{imgtitle:"형상관리도구 git",imgurl:"https://drive.google.com/file/d/1yLXiqHWblzxAnZXGZKvRcxU8HSgf3UqS/view?usp=drive_link",imglog:"프로그램의 전반적인 개발협업 및 버전관를 위한 도구",sourceurl:""})//이미지타이틀
 d2.set_content("테스트케이스를 작성합니다.")
 d2.set_content("단위테스트를 작성합니다.")
 
